4dcfae6e81e215a39d6b65ed3ac54bcb348b09fb
Carl Egbert <egbertcarl@gmail.com>
2023-10-28 17:39:32 -0700

    imitation is the sincerest form of flattery

--- 4.3/p44.rkt
+++ 4.3/p44.rkt
@@ -1,9 +1,17 @@
 #lang sicp
 
+(define (require condition)
+  (if (not condition)
+    (amb)))
+
+(define (distinct? items)
+  (cond ((null? items) #t)
+        ((null? (cdr items)) #t)
+        ((member (car items) (cdr items)) #f)
+        (else (distinct? (cdr items)))))
+
 (define col-symbols '(a b c d e f g h))
 (define row-symbols '(1 2 3 4 5 6 7 8))
-
-(define vect cons)
 
 (define (col q)
   (car q))
@@ -11,87 +19,53 @@
 (define (row q)
   (cdr q))
 
-(define (same-col qx qy)
-  (eq? (col qx) (col qy)))
-
-(define (same-row qx qy)
-  (eq? (row qx) (row qy)))
-
-(define diag-vectors
-  (list (vect 1 1)
-        (vect 1 -1)
-        (vect -1 1)
-        (vect -1 -1)))
-
-(define (add-vect sq vect)
-  (cons (+ (col vect) (col sq))
-        (+ (row vect) (row sq))))
-
-(define (oob? sq)
-  (or (= (col sq) -1)
-      (= (col sq) 8)
-      (= (row sq) -1)
-      (= (row sq) 8)))
-
-(define (diag-path start vect)
-  (let ((next-square (add-vect start vect)))
-    (if (oob? next-square)
-      '()
-      (cons next-square (diag-path next-square vect)))))
-
-(define (diag-paths q)
-  (map (lambda (vect)
-         (diag-path q vect))
-       diag-vectors))
-
-(define (any? predicate items)
-  (cond ((null? items) #f)
-        ((predicate? (car items)) #f)
-        (else (any predicate (cdr items)))))
-
-(define (same-diag? qx qy)
-  (any? (lambda (q) (eq? q qy))
-        (diag-paths qx)))
-
 (define (amb-range)
-  (amb (range 0 8)))
+  (amb 0 1 2 3 4 5 6 7))
 
-(define (amb-queen x)
-  (map cons
-       (amb-range)
-       (amb-range)))
+(define (amb-queen col)
+  (cons col (amb-range)))
 
-(define (every? pred items)
-  (if (null? items)
-    #t
-    (and (pred (car items))
-         (every? (cdr items)))))
+(define (up-diag queen)
+  (- (row queen) (col queen)))
 
-(define (check-queen q rest)
-  (if (null? rest)
-    #t
-    (and
-      (not
-        (or (any? (map (lambda (x) (same-diag q x))
-                       rest))
-            (any? (map (lambda (x) (same-col q x))
-                       rest))
-            (any? (map (lambda (x) (same-row q x))
-                       rest))))
-      (check-queen (car rest) (cdr rest)))))
+(define (down-diag queen)
+  (+ (row queen) (col queen)))
 
 (define (solve)
-  (let ((queens (map amb-queen (range 0 8))))
-    (require (distinct queens))
-    (require (every? (map check-queen)))
-    queens))
+  (define (iter queens)
+    ;; We get the distinct column for free thanks
+    ;; to only creating new queens on unoccupied
+    ;; columns.
+    ; (require (distinct? (map col queens)))
+
+    ;; No queen may share a row, of course.
+    (require (distinct? (map row queens)))
+
+    (require (distinct? (map up-diag queens)))
+    (require (distinct? (map down-diag queens)))
+
+    (if (= (length queens) 8)
+      queens
+      (iter (cons (amb-queen (length queens)) queens))))
+  (iter '()))
 
 (define (print-queens queens)
   (if (not (null? queens))
     (begin
       (display "(")
-      (display (list-ref (col (car queens)) col-symbols))
-      (display (list-ref (row (car queens)) row-symbols))
-      (display ")")
-      (newline)
+      (display (list-ref col-symbols (col (car queens))))
+      (display (list-ref row-symbols (row (car queens))))
+      (display ") ")
       (print-queens (cdr queens)))))
+
+(print-queens (solve))
+(newline)
+
+; --Q-----
+; -----Q--
+; ---Q----
+; -Q------
+; -------Q
+; ----Q---
+; ------Q-
+; Q-------
