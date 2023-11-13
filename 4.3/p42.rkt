55b9707aec0e61f48fd46cadfea18d24e65c096a
Carl Egbert <egbertcarl@gmail.com>
2023-11-12 17:16:17 -0800

    some 4.3 stuff

---
+++ 4.3/p42.rkt
@@ -0,0 +1,37 @@
+#lang sicp
+
+(define (require condition)
+  (if (not condition)
+    (amb)))
+
+(define (distinct? items)
+  (cond ((null? items) #t)
+        ((null? (cdr items)) #t)
+        ((member (car items) (cdr items)) #f)
+        (else (distinct? (cdr items)))))
+
+(define (xor a b)
+  (if a
+    (not b)
+    b))
+
+(define (solve)
+  (let ((betty (amb 1 2 3 4 5))
+        (ethel (amb 1 2 3 4 5))
+        (joan (amb 1 2 3 4 5))
+        (kitty (amb 1 2 3 4 5))
+        (mary (amb 1 2 3 4 5)))
+    (require (xor (= kitty 2) (= betty 3)))
+    (require (xor (= ethel 1) (= joan 2)))
+    (require (xor (= joan 3) (= ethel 5)))
+    (require (xor (= kitty 2) (= mary 4)))
+    (require (xor (= mary 4) (= betty 1)))
+    (require (distinct? (list betty ethel joan kitty mary)))
+
+    (list (list 'betty betty)
+          (list 'joan joan)
+          (list 'ethel ethel)
+          (list 'kitty kitty)
+          (list 'mary mary))))
+
+(solve)
---
+++ 4.3/p47.rkt
@@ -0,0 +1,2 @@
+;; it'd be infinite recursion if the amb evaluator ever
+;; moved past the initial (parse-word verbs).
